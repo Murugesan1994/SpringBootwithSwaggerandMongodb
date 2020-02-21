ce8e1ce6df6a7439a1e38bbd8017e8bfc0aa9f74
AZHAGU DEV <AZHAGU DEV@DESKTOP-6PLRMGU>
2020-02-21 16:59:40 +0530

    new commit

--- SpringwithMongodbSwagger/src/main/java/com/example/demo/SpringBootWithMongoDbApplication.java
+++ SpringwithMongodbSwagger/src/main/java/com/example/demo/SpringBootWithMongoDbApplication.java
@@ -9,37 +9,16 @@ import com.example.demo.repositories.UserRepository;
 
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses = com.example.demo.repositories.UserRepository.class)
+@EnableMongoRepositories(basePackageClasses = com.example.demo.repositories.EmployeeRepository.class)
 //@EnableJpaRepositories(basePackageClasses = com.example.demo.repositories.UserRepository.class)
 //@EnableSwagger2
 public class SpringBootWithMongoDbApplication {
 
 	public static void main(String[] args) {
+		//lol
 		SpringApplication.run(SpringBootWithMongoDbApplication.class, args);
 	}
 
-	/*
-	 * @Bean protected Docket swaggerConfiguration()
-	 * 
-	 * { return new Docket(DocumentationType.SWAGGER_2) .select()
-	 * .paths(PathSelectors.ant("/api/**"))
-	 * .apis(RequestHandlerSelectors.basePackage("com.example.demo")).build().
-	 * apiInfo(apiDetails());
-	 * 
-	 * 
-	 * } private ApiInfo apiDetails() { return new
-	 * ApiInfo("Mongo-DB-","This Project about spring boot with MongoDb","Sample",
-	 * "HI", new springfox.documentation.service.Contact("Manikandan","Diatoz.com",
-	 * "abc.gmail.com"), "API ","Swagger.api", Collections.emptyList());
-	 * 
-	 * }
-	 * 
-	 * @Bean Docket SwaggerConfiguration() { return new
-	 * Docket(DocumentationType.SWAGGER_2) .select()
-	 * .paths(PathSelectors.ant("/api/*"))
-	 * .apis(RequestHandlerSelectors.basePackage("com.example.demo")).build();
-	 * 
-	 * }
-	 */
+	
 
 }
